d34c9e0c7f70137f71ecd19c0aa40a8937e30483
slowdata <slowmotion@outlook.com>
2019-03-25 15:43:13 +0000

    Create teams

--- src/components/team.js
+++ src/components/team.js
@@ -2,21 +2,23 @@ import React from "react"
 
 import styles from "./team.module.css"
 
-const Team = ({ number, players }) => (
+const Team = ({ number, players, deletePlayer, switchPlayer }) => (
   <div className={styles.card}>
     <header>{`team ${number}`}</header>
     <ul>
       {players.length > 0 &&
         players.map(player => {
           return number === 1 ? (
-            <li key={`${Date.now()}${player}`}>
-              {player}
-              <button>-></button>
+            <li key={player.id}>
+              {player.name}
+              <button onClick={() => deletePlayer(player.id)}>X</button>
+              <button onClick={() => switchPlayer(player.id)}>{"->"}</button>
             </li>
           ) : (
-            <li key={`${Date.now()}${player}`}>
-              <button>{"<-"}</button>
-              {player}
+            <li key={player.id}>
+              <button onClick={() => switchPlayer(player.id)}>{"<-"}</button>
+              <button onClick={() => deletePlayer(player.id)}>{"X"}</button>
+              {player.name}
             </li>
           )
         })}
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,8 @@ class Index extends Component {
     name: "",
     team1: [],
     team2: [],
-    error: false,
+    error: 0,
+    msg: "",
   }
 
   handleInputChange = e => {
@@ -19,34 +20,120 @@ class Index extends Component {
   }
 
   addPlayer = e => {
-    console.log(e)
     e.preventDefault()
     const { team1, team2, name } = this.state
-    if (team1.length <= 7) {
-      const newTeam = team1.concat([...name])
-      this.setState({ team1: newTeam, name: "" })
-    } else if (team2 <= 7) {
-      const newTeam = team2.concat([...name])
-      this.setState({ team2: newTeam, name: "" })
+    let msg = "Estás no bom caminho!! Mas ainda falta."
+    let error = 1
+    if (name) {
+      if (team1.length < 7) {
+        const player = { name, id: Date.now() }
+        const newTeam = team1.concat([player])
+        if (newTeam.length === 7 && team2.length === 7) {
+          error = 2
+          msg = "Conseguiste!! O Tiago vai ficar orgulhoso"
+        }
+        this.setState({ team1: newTeam, name: "", error, msg })
+      } else if (team2.length < 7) {
+        const player = { name, id: Date.now() }
+        const newTeam = team2.concat([player])
+        if (newTeam.length === 7 && team1.length === 7) {
+          error = 2
+          msg = "Conseguiste!! O Tiago vai ficar orgulhoso"
+        }
+        this.setState({ team2: newTeam, name: "", error, msg })
+      } else {
+        this.setState({
+          error: 2,
+          msg: "Conseguiste!! O Tiago vai ficar orgulhoso",
+        })
+      }
+    }
+  }
+
+  handleDelete = id => {
+    const { team1, team2 } = this.state
+    let player = team1.find(player => player.id === id)
+    let msg = "Estás no bom caminho!! Mas ainda falta."
+    let error = 0
+    if (player) {
+      const team = team1.filter(player => player.id !== id)
+      if (team.length > 0) error = 1
+      this.setState({ team1: team, error, msg })
     } else {
-      this.setState({ error: true })
+      player = team2.find(player => player.id === id)
+      if (player) {
+        const team = team2.filter(player => player.id !== id)
+        if (team.length > 0) error = 1
+        this.setState({ team2: team, error, msg })
+      } else {
+        this.setState({ error })
+      }
+    }
+  }
+
+  handleSwitchPlayer = id => {
+    const { team1, team2 } = this.state
+    const index = team1.findIndex(player => player.id === id)
+    if (index > -1) {
+      const p1 = team1[index]
+      const p2 = team2[index]
+      if (p2) {
+        team1[index] = p2
+        team2[index] = p1
+        this.setState({ team1, team2 })
+      }
+    } else {
+      const index = team2.findIndex(player => player.id === id)
+      const p1 = team1[index]
+      const p2 = team2[index]
+      if (p1) {
+        team1[index] = p2
+        team2[index] = p1
+        this.setState({ team1, team2 })
+      }
     }
   }
 
   render() {
-    const { team1, team2, name } = this.state
+    const { team1, team2, name, error, msg } = this.state
     return (
       <>
-        <h2 className={styles.title}>Por favor faça as suas equipas</h2>
+        <h2 className={styles.title}>O Tiago manda fazer as equipas!</h2>
         <Input
           name={name}
           handleChange={this.handleInputChange}
           handleSubmit={this.addPlayer}
         />
         <div className={styles.container}>
-          <Team number={1} players={team1} />
-          <Team number={2} players={team2} />
+          <Team
+            number={1}
+            players={team1}
+            deletePlayer={this.handleDelete}
+            switchPlayer={this.handleSwitchPlayer}
+          />
+          <Team
+            number={2}
+            players={team2}
+            deletePlayer={this.handleDelete}
+            switchPlayer={this.handleSwitchPlayer}
+          />
         </div>
+        {error > 0 &&
+          (error > 1 ? (
+            <div className={styles.msgOk}>
+              {msg} 
+              <span role="img" aria-label="blink with thung emoji">
+                😜
+              </span>
+            </div>
+          ) : (
+            <div className={styles.msgError}>
+              {msg} 
+              <span role="img" aria-label="blink with thung emoji">
+                😜
+              </span>
+            </div>
+          ))}
       </>
     )
   }
